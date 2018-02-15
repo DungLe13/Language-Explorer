2e307aa521b060461761756fceb4304491ccd481
DungLe13 <dungle@bennington.edu>
2018-02-15 01:00:18 -0500

    Features added

---
+++ Language Adventure/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public float turnSpeed = 5;
+    public GameObject player;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        //offset = new Vector3(player.transform.position.x - 5, player.transform.position.y + 3, player.transform.position.z + 10);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //offset = Quaternion.AngleAxis(Input.GetAxisRaw("Horizontal") * turnSpeed, Vector3.up) * offset;
+        transform.position = player.transform.position + offset;
+        //transform.LookAt(player.transform.position);
+    }
+}
---
+++ Language Adventure/Assets/Scripts/Elf_Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elf_Movement : MonoBehaviour {
+    Rigidbody physicBody;
+    public float speed = 5;
+
+    // Use this for initialization
+    void Start () {
+        physicBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        transform.Rotate(0, horizontalInput * speed, 0);
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.forward * Time.deltaTime * speed;
+        }
+    }
+}
